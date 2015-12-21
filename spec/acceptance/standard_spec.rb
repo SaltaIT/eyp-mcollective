0956d8e8f6e8c382d7e99e032117b7ec0aa28059
Jordi Prats <jprats@systemadmin.es>
2015-12-21 12:44:50 +0100

    test fllo

--- spec/acceptance/standard_spec.rb
+++ spec/acceptance/standard_spec.rb
@@ -4,7 +4,7 @@ describe 'mcollective class' do
 
   context 'basic setup (mcollective client with activemq)' do
     # Using puppet_apply as a helper
-    it 'should work with no errors based on the example' do
+    it 'should work with no errors' do
       pp = <<-EOF
 
       class { 'mcollective':
@@ -32,7 +32,7 @@ describe 'mcollective class' do
 
   context 'different stomp port (mcollective client with activemq)' do
     # Using puppet_apply as a helper
-    it 'should work with no errors based on the example' do
+    it 'should work with no errors' do
       pp = <<-EOF
 
       class { 'mcollective':
@@ -59,4 +59,35 @@ describe 'mcollective class' do
       expect(shell("sleep 60; mco ping -t 5 --dt 5 --connection-timeout 5").exit_code).to be_zero
     end
   end
+
+  context 'mcollective connecting to a closed port' do
+    # Using puppet_apply as a helper
+    it 'should work with no errors' do
+      pp = <<-EOF
+
+      class { 'mcollective':
+        password => 'aaa',
+        hostname => 'localhost',
+        agent=>true,
+        client=>true,
+        stomp_port=>'9999',
+      }
+
+      class { 'mcollective::activemq':
+        adminpw => 'admin',
+        userpw => 'aaa',
+        stomp_port=>'8888',
+      }
+      EOF
+
+      # Run it twice and test for idempotency
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    it 'mco ping' do
+      expect(shell("sleep 60; mco ping -t 5 --dt 5 --connection-timeout 5").exit_code).to_not be_zero
+    end
+  end
+
 end
