d4568ee75474de6d1844e73281b2abe20107d853
Jordi Prats <jprats@systemadmin.es>
2015-12-31 11:52:55 +0100

    fora tabs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,227 +1,227 @@
-class mcollective	(
-				$connector=$mcollective::params::collector_default,
-				$username="mcollective",
-				$password,
-				$hostname,
-				$stomp_port=$mcollective::params::stomp_port_default,
-				$psk=$mcollective::params::default_psk,
-				$customfactspattern=undef,
-				$customfactsfile='/etc/mcollective/facts.yaml',
-				$subcollectives=undef,
-				$ensure='installed',
-				$agent=true,
-				$client=false,
-				$plugins_packages = [ 'package', 'service', 'puppet' ],
-				$plugins_packages_ensure='present',
-				$custom_plugins = [ 'rmrf' ],
+class mcollective  (
+        $connector=$mcollective::params::collector_default,
+        $username="mcollective",
+        $password,
+        $hostname,
+        $stomp_port=$mcollective::params::stomp_port_default,
+        $psk=$mcollective::params::default_psk,
+        $customfactspattern=undef,
+        $customfactsfile='/etc/mcollective/facts.yaml',
+        $subcollectives=undef,
+        $ensure='installed',
+        $agent=true,
+        $client=false,
+        $plugins_packages = [ 'package', 'service', 'puppet' ],
+        $plugins_packages_ensure='present',
+        $custom_plugins = [ 'rmrf' ],
 
-			) inherits mcollective::params {
+      ) inherits mcollective::params {
 
-	validate_string($connector)
-	validate_string($username)
-	validate_string($password)
-	validate_string($hostname)
-	validate_string($psk)
+  validate_string($connector)
+  validate_string($username)
+  validate_string($password)
+  validate_string($hostname)
+  validate_string($psk)
 
-	validate_re($connector, [ '^activemq$' ], "Not a supported connector: $version")
+  validate_re($connector, [ '^activemq$' ], "Not a supported connector: $version")
 
-	validate_re($ensure, [ '^installed$', '^latest$' ], "Not a valid package status: ${package_status}")
+  validate_re($ensure, [ '^installed$', '^latest$' ], "Not a valid package status: ${package_status}")
 
 
-	if($subcollectives)
-	{
-		validate_array($subcollectives)
-	}
+  if($subcollectives)
+  {
+    validate_array($subcollectives)
+  }
 
-	Exec {
-		path => '/sbin:/bin:/usr/sbin:/usr/bin',
-	}
+  Exec {
+    path => '/sbin:/bin:/usr/sbin:/usr/bin',
+  }
 
-	exec { "mkdir -p ${libdir} mcollective agent":
-		command => "mkdir -p ${libdir}/mcollective/agent",
-		creates => "${libdir}/mcollective/agent",
-		require => Package[$mcollectiveagentpackages],
-	}
+  exec { "mkdir -p ${libdir} mcollective agent":
+    command => "mkdir -p ${libdir}/mcollective/agent",
+    creates => "${libdir}/mcollective/agent",
+    require => Package[$mcollectiveagentpackages],
+  }
 
-	if ! defined(Package['puppetlabs-release'])
-	{
-		package { 'puppetlabs-release':
-			ensure   => 'installed',
-			provider => $mcollective::params::puppetlabspackageprovider,
-			source   => $mcollective::params::puppetlabspackage,
-			notify   => Exec['update puppetlabs repo'],
-		}
-	}
+  if ! defined(Package['puppetlabs-release'])
+  {
+    package { 'puppetlabs-release':
+      ensure   => 'installed',
+      provider => $mcollective::params::puppetlabspackageprovider,
+      source   => $mcollective::params::puppetlabspackage,
+      notify   => Exec['update puppetlabs repo'],
+    }
+  }
 
-	if($mcollective::params::puppetlabspackageprovider=="dpkg")
-	{
-		exec { 'update puppetlabs repo':
-			command     => 'apt-get update',
-			require     => Package['puppetlabs-release'],
-			refreshonly => true,
-		}
-	}
-	else
-	{
-		exec { 'update puppetlabs repo':
-			command     => 'echo systemadmin.es - best blog ever',
-			require     => Package['puppetlabs-release'],
-			refreshonly => true,
-		}
-	}
+  if($mcollective::params::puppetlabspackageprovider=="dpkg")
+  {
+    exec { 'update puppetlabs repo':
+      command     => 'apt-get update',
+      require     => Package['puppetlabs-release'],
+      refreshonly => true,
+    }
+  }
+  else
+  {
+    exec { 'update puppetlabs repo':
+      command     => 'echo systemadmin.es - best blog ever',
+      require     => Package['puppetlabs-release'],
+      refreshonly => true,
+    }
+  }
 
-	package { $mcollectiveagentpackages:
-		ensure  => $ensure,
-		require => Exec['update puppetlabs repo'],
-		notify  => $notify_service_mcollective,
-	}
+  package { $mcollectiveagentpackages:
+    ensure  => $ensure,
+    require => Exec['update puppetlabs repo'],
+    notify  => $notify_service_mcollective,
+  }
 
-	if($agent)
-	{
+  if($agent)
+  {
 
-		if($plugins_packages)
-		{
-			$agent_plugin_packages=suffix(prefix($plugins_packages, 'mcollective-'), '-agent')
-			$agent_plugin_packages_common=suffix(prefix($plugins_packages, 'mcollective-'), '-common')
+    if($plugins_packages)
+    {
+      $agent_plugin_packages=suffix(prefix($plugins_packages, 'mcollective-'), '-agent')
+      $agent_plugin_packages_common=suffix(prefix($plugins_packages, 'mcollective-'), '-common')
 
-			package { $agent_plugin_packages:
-				ensure  => $plugins_packages_ensure,
-				require => Package[$mcollectiveagentpackages],
-				notify  => Service[$mcollectiveagentservice],
-			}
+      package { $agent_plugin_packages:
+        ensure  => $plugins_packages_ensure,
+        require => Package[$mcollectiveagentpackages],
+        notify  => Service[$mcollectiveagentservice],
+      }
 
-			ensure_packages($agent_plugin_packages_common,
-				{
-					ensure  => $plugins_packages_ensure,
-					require => Package[$mcollectiveagentpackages],
-					notify  => Service[$mcollectiveagentservice],
-				}
-			)
-		}
+      ensure_packages($agent_plugin_packages_common,
+        {
+          ensure  => $plugins_packages_ensure,
+          require => Package[$mcollectiveagentpackages],
+          notify  => Service[$mcollectiveagentservice],
+        }
+      )
+    }
 
-		#agent rmrf
-		if member($custom_plugins, 'rmrf')
-		{
+    #agent rmrf
+    if member($custom_plugins, 'rmrf')
+    {
 
-			file { "${libdir}/mcollective/agent/rmrf.rb":
-				ensure   => 'present',
-				owner    => 'root',
-				group    => 'root',
-				mode     => '0644',
-				source 	 => "puppet:///modules/${module_name}/rmrf/rmrf.rb",
-				notify   => Service[$mcollectiveagentservice],
-				require  => [Exec["mkdir -p ${libdir} mcollective agent"], Package[$mcollectiveagentpackages]],
-			}
+      file { "${libdir}/mcollective/agent/rmrf.rb":
+        ensure   => 'present',
+        owner    => 'root',
+        group    => 'root',
+        mode     => '0644',
+        source    => "puppet:///modules/${module_name}/rmrf/rmrf.rb",
+        notify   => Service[$mcollectiveagentservice],
+        require  => [Exec["mkdir -p ${libdir} mcollective agent"], Package[$mcollectiveagentpackages]],
+      }
 
-			if ! defined(File["${libdir}/mcollective/agent/rmrf.ddl"])
-			{
-				file { "${libdir}/mcollective/agent/rmrf.ddl":
-					ensure   => 'present',
-					owner    => 'root',
-					group    => 'root',
-					mode     => '0644',
-					source 	 => "puppet:///modules/${module_name}/rmrf/rmrf.ddl",
-					notify   => Service[$mcollectiveagentservice],
-					require  => [Exec["mkdir -p ${libdir} mcollective agent"], Package[$mcollectiveagentpackages]],
-				}
-			}
+      if ! defined(File["${libdir}/mcollective/agent/rmrf.ddl"])
+      {
+        file { "${libdir}/mcollective/agent/rmrf.ddl":
+          ensure   => 'present',
+          owner    => 'root',
+          group    => 'root',
+          mode     => '0644',
+          source    => "puppet:///modules/${module_name}/rmrf/rmrf.ddl",
+          notify   => Service[$mcollectiveagentservice],
+          require  => [Exec["mkdir -p ${libdir} mcollective agent"], Package[$mcollectiveagentpackages]],
+        }
+      }
 
-		}
+    }
 
-		file { '/etc/mcollective/server.cfg':
-			ensure  => 'present',
-			owner   => 'root',
-			group   => 'root',
-			mode    => '0644',
-			require => Package[$mcollectiveagentpackages],
-			notify  => Service[$mcollectiveagentservice],
-			content => template("mcollective/agentconf.erb")
-		}
+    file { '/etc/mcollective/server.cfg':
+      ensure  => 'present',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => Package[$mcollectiveagentpackages],
+      notify  => Service[$mcollectiveagentservice],
+      content => template("mcollective/agentconf.erb")
+    }
 
-		if($customfactspattern)
-		{
-			#facter -py | grep
-			exec { 'customfacts':
-				command => "facter -py | grep '${customfactspattern}' > ${customfactsfile}",
-				notify  => Service[$mcollectiveagentservice],
-				require => File['/etc/mcollective/server.cfg'],
-			}
-		}
-		else
-		{
-			#facter -py
-			exec { 'customfacts':
-				command => "facter -py > ${customfactsfile}",
-				notify  => Service[$mcollectiveagentservice],
-				require => File['/etc/mcollective/server.cfg'],
-			}
-		}
+    if($customfactspattern)
+    {
+      #facter -py | grep
+      exec { 'customfacts':
+        command => "facter -py | grep '${customfactspattern}' > ${customfactsfile}",
+        notify  => Service[$mcollectiveagentservice],
+        require => File['/etc/mcollective/server.cfg'],
+      }
+    }
+    else
+    {
+      #facter -py
+      exec { 'customfacts':
+        command => "facter -py > ${customfactsfile}",
+        notify  => Service[$mcollectiveagentservice],
+        require => File['/etc/mcollective/server.cfg'],
+      }
+    }
 
-		service { $mcollectiveagentservice:
-			enable  => true,
-			ensure  => "running",
-			require => Exec['customfacts'],
-		}
-	}
+    service { $mcollectiveagentservice:
+      enable  => true,
+      ensure  => "running",
+      require => Exec['customfacts'],
+    }
+  }
 
-	if($client)
-	{
-		if($mcollectiveclientpackages!=undef)
-		{
-			package { $mcollectiveclientpackages:
-				ensure => 'installed',
-			}
-		}
+  if($client)
+  {
+    if($mcollectiveclientpackages!=undef)
+    {
+      package { $mcollectiveclientpackages:
+        ensure => 'installed',
+      }
+    }
 
-		if(! $agent)
-		{
-			if member($custom_plugins, 'rmrf')
-			{
-				#sanity check
-				if ! defined(File["${libdir}/mcollective/agent/rmrf.ddl"])
-				{
-					file { "${libdir}/mcollective/agent/rmrf.ddl":
-						ensure   => 'present',
-						owner    => 'root',
-						group    => 'root',
-						mode     => '0644',
-						source 	 => "puppet:///modules/${module_name}/rmrf/rmrf.ddl",
-						require  => Exec["mkdir -p ${libdir} mcollective agent"],
-					}
-				}
-			}
-		}
-		else
-		{
-			$notify_service_mcollective=Service[$mcollectiveagentservice]
-		}
+    if(! $agent)
+    {
+      if member($custom_plugins, 'rmrf')
+      {
+        #sanity check
+        if ! defined(File["${libdir}/mcollective/agent/rmrf.ddl"])
+        {
+          file { "${libdir}/mcollective/agent/rmrf.ddl":
+            ensure   => 'present',
+            owner    => 'root',
+            group    => 'root',
+            mode     => '0644',
+            source    => "puppet:///modules/${module_name}/rmrf/rmrf.ddl",
+            require  => Exec["mkdir -p ${libdir} mcollective agent"],
+          }
+        }
+      }
+    }
+    else
+    {
+      $notify_service_mcollective=Service[$mcollectiveagentservice]
+    }
 
-		if($plugins_packages)
-		{
-			$client_plugin_packages=suffix(prefix($plugins_packages, 'mcollective-'), '-client')
-			$client_plugin_packages_common=suffix(prefix($plugins_packages, 'mcollective-'), '-common')
+    if($plugins_packages)
+    {
+      $client_plugin_packages=suffix(prefix($plugins_packages, 'mcollective-'), '-client')
+      $client_plugin_packages_common=suffix(prefix($plugins_packages, 'mcollective-'), '-common')
 
-			package { $client_plugin_packages:
-				ensure  => $plugins_packages_ensure,
-				require => Package[$mcollectiveagentpackages],
-			}
+      package { $client_plugin_packages:
+        ensure  => $plugins_packages_ensure,
+        require => Package[$mcollectiveagentpackages],
+      }
 
-			ensure_packages($client_plugin_packages_common,
-				{
-					ensure  => $plugins_packages_ensure,
-					require => Package[$mcollectiveagentpackages],
-					notify  => $notify_service_mcollective,
-				}
-			)
-		}
+      ensure_packages($client_plugin_packages_common,
+        {
+          ensure  => $plugins_packages_ensure,
+          require => Package[$mcollectiveagentpackages],
+          notify  => $notify_service_mcollective,
+        }
+      )
+    }
 
-		file { '/etc/mcollective/client.cfg':
-			ensure  => 'present',
-			owner   => 'root',
-			group   => 'root',
-			mode    => '0644',
-			require => Package[$mcollectiveagentpackages],
-			content => template("${module_name}/clientconf.erb")
-		}
-	}
+    file { '/etc/mcollective/client.cfg':
+      ensure  => 'present',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => Package[$mcollectiveagentpackages],
+      content => template("${module_name}/clientconf.erb")
+    }
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,99 +1,99 @@
 class mcollective::params {
 
-	$collector_default="activemq"
-	$default_psk="viscalaterralliure"
+  $collector_default="activemq"
+  $default_psk="viscalaterralliure"
 
-	$stomp_port_default='6163'
+  $stomp_port_default='6163'
 
-	case $::osfamily
-	{
-		'redhat':
+  case $::osfamily
+  {
+    'redhat':
     {
-			case $::operatingsystemrelease
-			{
-				/^6.*$/:
-				{
-					$libdir='/usr/libexec/mcollective'
+      case $::operatingsystemrelease
+      {
+        /^6.*$/:
+        {
+          $libdir='/usr/libexec/mcollective'
 
-					#mcollective agent
+          #mcollective agent
 
-					$mcollectiveagentpackages= [ 'mcollective', 'rubygem-stomp' ]
-					$mcollectiveagentservice='mcollective'
-					$puppetlabspackage='http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm'
-					$puppetlabspackageprovider='rpm'
+          $mcollectiveagentpackages= [ 'mcollective', 'rubygem-stomp' ]
+          $mcollectiveagentservice='mcollective'
+          $puppetlabspackage='http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm'
+          $puppetlabspackageprovider='rpm'
 
-					#activemq
-					$activemqpackages = [ 'activemq' ]
-					$activemq_conf='/etc/activemq/activemq.xml'
-					$activemq_conf_template='activemqconf_rh.erb'
+          #activemq
+          $activemqpackages = [ 'activemq' ]
+          $activemq_conf='/etc/activemq/activemq.xml'
+          $activemq_conf_template='activemqconf_rh.erb'
 
-					$activemq_baseconf='/etc/activemq'
+          $activemq_baseconf='/etc/activemq'
 
-					$activemq_jetty="${activemq_baseconf}/jetty.xml"
-					$activemq_jetty_template='activemq_jetty.erb'
+          $activemq_jetty="${activemq_baseconf}/jetty.xml"
+          $activemq_jetty_template='activemq_jetty.erb'
 
-					$activemq_jetty_realm="${activemq_baseconf}/jetty-realm.properties"
-					$activemq_jetty_realm_template='activemq_jetty_realm.erb'
+          $activemq_jetty_realm="${activemq_baseconf}/jetty-realm.properties"
+          $activemq_jetty_realm_template='activemq_jetty_realm.erb'
 
-					$activemq_credentials="${activemq_baseconf}/credentials.properties"
-					$activemq_credentials_template='activemq_credentials.erb'
+          $activemq_credentials="${activemq_baseconf}/credentials.properties"
+          $activemq_credentials_template='activemq_credentials.erb'
 
-					$activemq_servicefile=$activemq_conf
-				}
-				default: { fail("Unsupported RHEL/CentOS version! - $::operatingsystemrelease")  }
-			}
+          $activemq_servicefile=$activemq_conf
+        }
+        default: { fail("Unsupported RHEL/CentOS version! - $::operatingsystemrelease")  }
+      }
 
-		}
-		'Debian':
-		{
-			case $::operatingsystem
-			{
-				'Ubuntu':
-				{
-					case $::operatingsystemrelease
-					{
-						/^14.*$/:
-						{
-							$libdir='/usr/share/mcollective/plugins'
+    }
+    'Debian':
+    {
+      case $::operatingsystem
+      {
+        'Ubuntu':
+        {
+          case $::operatingsystemrelease
+          {
+            /^14.*$/:
+            {
+              $libdir='/usr/share/mcollective/plugins'
 
-							#mcollecitve agent
+              #mcollecitve agent
 
-							$mcollectiveagentpackages= [ 'mcollective', 'ruby-stomp' ]
-							$mcollectiveclientpackages= [ 'mcollective-client' ]
-							$mcollectiveagentservice='mcollective'
-							$puppetlabspackage='http://apt.puppetlabs.com/puppetlabs-release-trusty.deb'
-							$puppetlabspackageprovider='dpkg'
+              $mcollectiveagentpackages= [ 'mcollective', 'ruby-stomp' ]
+              $mcollectiveclientpackages= [ 'mcollective-client' ]
+              $mcollectiveagentservice='mcollective'
+              $puppetlabspackage='http://apt.puppetlabs.com/puppetlabs-release-trusty.deb'
+              $puppetlabspackageprovider='dpkg'
 
-							#activemq
-							$activemqpackages = [ 'activemq' ]
-							$activemq_conf='/etc/activemq/instances-available/main/activemq.xml'
-							$activemq_conf_template='activemqconf_ubuntu14.erb'
+              #activemq
+              $activemqpackages = [ 'activemq' ]
+              $activemq_conf='/etc/activemq/instances-available/main/activemq.xml'
+              $activemq_conf_template='activemqconf_ubuntu14.erb'
 
-							$activemq_ln='/etc/activemq/instances-enabled/main'
-							$activemq_lndest='/etc/activemq/instances-available/main'
-							$activemq_baseconf=$activemq_lndest
+              $activemq_ln='/etc/activemq/instances-enabled/main'
+              $activemq_lndest='/etc/activemq/instances-available/main'
+              $activemq_baseconf=$activemq_lndest
 
-							$activemq_sysconf='/etc/default/activemq'
-							$activemq_sysconf_template='sysconf_debian.erb'
+              $activemq_sysconf='/etc/default/activemq'
+              $activemq_sysconf_template='sysconf_debian.erb'
 
-							$activemq_jetty="${activemq_baseconf}/jetty.xml"
-							$activemq_jetty_template='activemq_jetty.erb'
+              $activemq_jetty="${activemq_baseconf}/jetty.xml"
+              $activemq_jetty_template='activemq_jetty.erb'
 
-							$activemq_jetty_realm="${activemq_baseconf}/jetty-realm.properties"
-							$activemq_jetty_realm_template='activemq_jetty_realm.erb'
+              $activemq_jetty_realm="${activemq_baseconf}/jetty-realm.properties"
+              $activemq_jetty_realm_template='activemq_jetty_realm.erb'
 
-							$activemq_credentials="${activemq_baseconf}/credentials.properties"
-							$activemq_credentials_template='activemq_credentials.erb'
+              $activemq_credentials="${activemq_baseconf}/credentials.properties"
+              $activemq_credentials_template='activemq_credentials.erb'
 
-							$activemq_servicefile=$activemq_ln
-						}
-						default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
-					}
-				}
-				'Debian': { fail("Unsupported")  }
-				default: { fail("Unsupported Debian flavour!")  }
-			}
-		}
-		default: { fail("Unsupported OS!")  }
-	}
+              $activemq_servicefile=$activemq_ln
+            }
+            default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
+          }
+        }
+        'Debian': { fail("Unsupported")  }
+        default: { fail("Unsupported Debian flavour!")  }
+      }
+    }
+    default: { fail("Unsupported OS!")  }
+  }
 }
