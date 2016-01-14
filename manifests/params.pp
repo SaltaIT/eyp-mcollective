5bed18fb5b9990894ae827bba38c419d51622608
Jordi Prats <jprats@systemadmin.es>
2016-01-14 11:31:57 +0100

    afegides proves centos 6 i 7

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,36 +9,45 @@ class mcollective::params {
   {
     'redhat':
     {
+      $libdir='/usr/libexec/mcollective'
+
+      #mcollective agent
+
+      $mcollectiveagentpackages= [ 'mcollective', 'rubygem-stomp' ]
+      $mcollectiveagentservice='mcollective'
+      $puppetlabspackageprovider='rpm'
+
+      #activemq
+      $activemqpackages = [ 'activemq' ]
+      $activemq_conf='/etc/activemq/activemq.xml'
+      $activemq_conf_template='activemqconf_rh.erb'
+
+      $activemq_baseconf='/etc/activemq'
+
+      $activemq_jetty="${activemq_baseconf}/jetty.xml"
+      $activemq_jetty_template='activemq_jetty.erb'
+
+      $activemq_jetty_realm="${activemq_baseconf}/jetty-realm.properties"
+      $activemq_jetty_realm_template='activemq_jetty_realm.erb'
+
+      $activemq_credentials="${activemq_baseconf}/credentials.properties"
+      $activemq_credentials_template='activemq_credentials.erb'
+
+      $activemq_servicefile=$activemq_conf
+
       case $::operatingsystemrelease
       {
+        /^5.*$/:
+        {
+          $puppetlabspackage='http://yum.puppetlabs.com/puppetlabs-release-el-5.noarch.rpm'
+        }
         /^6.*$/:
         {
-          $libdir='/usr/libexec/mcollective'
-
-          #mcollective agent
-
-          $mcollectiveagentpackages= [ 'mcollective', 'rubygem-stomp' ]
-          $mcollectiveagentservice='mcollective'
           $puppetlabspackage='http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm'
-          $puppetlabspackageprovider='rpm'
-
-          #activemq
-          $activemqpackages = [ 'activemq' ]
-          $activemq_conf='/etc/activemq/activemq.xml'
-          $activemq_conf_template='activemqconf_rh.erb'
-
-          $activemq_baseconf='/etc/activemq'
-
-          $activemq_jetty="${activemq_baseconf}/jetty.xml"
-          $activemq_jetty_template='activemq_jetty.erb'
-
-          $activemq_jetty_realm="${activemq_baseconf}/jetty-realm.properties"
-          $activemq_jetty_realm_template='activemq_jetty_realm.erb'
-
-          $activemq_credentials="${activemq_baseconf}/credentials.properties"
-          $activemq_credentials_template='activemq_credentials.erb'
-
-          $activemq_servicefile=$activemq_conf
+        }
+        /^7.*$/:
+        {
+          $puppetlabspackage='http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm'
         }
         default: { fail("Unsupported RHEL/CentOS version! - $::operatingsystemrelease")  }
       }
